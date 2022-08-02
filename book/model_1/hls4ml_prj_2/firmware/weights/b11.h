820fecc570d0a007480362e5c529dd5a915d3d0e
William McCormack <williammccormack@Williams-MacBook-Pro.local>
2022-08-01 20:43:37 -0400

    adding the model files from 2.2

---
+++ book/model_1/hls4ml_prj_2/firmware/weights/b11.h
@@ -0,0 +1,15 @@
+//Numpy array shape [5]
+//Min -0.139161720872
+//Max 0.062349751592
+//Number of zeros 0
+
+#ifndef B11_H_
+#define B11_H_
+
+#ifndef __SYNTHESIS__
+output_bias_t b11[5];
+#else
+output_bias_t b11[5] = {-0.139162, 0.058128, 0.062350, 0.013011, -0.005964};
+#endif
+
+#endif
---
+++ book/model_2/hls4ml_prj_1/firmware/weights/b11.h
@@ -0,0 +1,15 @@
+//Numpy array shape [5]
+//Min -0.102692782879
+//Max 0.118463508785
+//Number of zeros 0
+
+#ifndef B11_H_
+#define B11_H_
+
+#ifndef __SYNTHESIS__
+output_bias_t b11[5];
+#else
+output_bias_t b11[5] = {-0.001935, 0.044482, -0.017264, -0.102693, 0.118464};
+#endif
+
+#endif
